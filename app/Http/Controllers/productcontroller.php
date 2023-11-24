e5d359cfbb8d56a67c6a84d3bc38d11b8ef616f9
Novan Nur Bhakti <novan.bhakti@binus.ac.id>
2023-11-24 14:12:32 +0700

    initial commit

---
+++ app/Http/Controllers/productcontroller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\barang;
+use Illuminate\Http\Request;
+
+class productcontroller extends Controller
+{
+    public function index()
+    {
+       $barangs = barang::paginate(8)->withQuerystring();
+        // return view('pages.product',compact('barangs'),[
+        //     'title' => 'Product'
+        // ])->render();
+
+        return response()->json($barangs, 200);
+    }
+
+    public function details($id){
+        return view('pages.productdetails', [
+            'title' => 'Product',
+            'barang' => barang::findOrFail($id),
+        ]);
+    }
+
+    public function search(Request $request){
+        $barangs = barang::where('namabarang','LIKE',"%$request->search%")->paginate(8)->withQuerystring();
+
+        return view('pages.product',[
+            'title' => 'Search Product'
+        ])->with('barangs',$barangs);
+
+    }
+}
